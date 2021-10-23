d0a9957f8c0c44ee734ff5358154669d6de52feb
turbotyla <turbotylar@gmail.com>
2021-10-24 09:34:56 +1300

    Changed to asyncpraw

--- cogs/redditgive.py
+++ cogs/redditgive.py
@@ -6,7 +6,6 @@ class RedditGive(commands.Cog, name="Reddit Give"):
     def __init__(self, client):
         self.client = client
 
-
     @commands.command(
         name="give",
         breif="give a image from reddit",
@@ -15,18 +14,18 @@ class RedditGive(commands.Cog, name="Reddit Give"):
     async def give(self, ctx, arg):
         try:
             if arg == 'loli':
-                praw_login('cutelittlefangs')
+                await (praw_login('cutelittlefangs'))
                 await ctx.send("Here is your loli: <:Lozic:854911358114332683>")
                 await ctx.send(file=discord.File('image.png'))
             elif arg == 'cat':
-                praw_login('cat')
+                await praw_login('cat')
                 await ctx.send("Here is your cat:")
                 await ctx.send(file=discord.File('image.png'))
             elif arg == 'kat':
-                praw_login('KatarinaMains')
+                await praw_login('KatarinaMains')
                 await ctx.send("Here is your cat:")
             elif arg == 'yuumi':
-                praw_login('YuumiMains')
+                await praw_login('YuumiMains')
                 await ctx.send("Here is your cat:")
                 await ctx.send(file=discord.File('image.png')) 
             else:
--- praw_.py
+++ praw_.py
@@ -1,48 +1,32 @@
-def praw_login(subreddit):
-    import praw
+async def praw_login(post):
+    import asyncpraw as praw
     import urllib
     import urllib.request
-    import json
-
+    import json        
     
-    with open("config.json") as f:
-        config = json.load(f)
-
     filename = ('image.png')
     found = False
-    
+    with open("config.json") as f:
+        config = json.load(f)
     reddit = praw.Reddit(client_id='oVD8qBhq5Ab3Wg',
                         client_secret=config["praw_secret"], password=config["praw_password"],
                         user_agent='Rtheif', username="Mr_Turbotylar")
-
     sent = []
-
     try:
             with open("sent.json", "r") as f:
                 sent = json.load(f)
     except:
             pass
-
     while not found:
-
-        
-
-            for post in reddit.subreddit(subreddit).hot():
-                if post.permalink in sent:
-                    #print("Scam")
-                    continue
-            
-                url = (post.url)
-
-                sent.append(post.permalink)
-
-                with open("sent.json", "w") as f:
-                    json.dump(sent, f)
-                
-                
-                if url.endswith("jpg") or url.endswith("jpeg") or url.endswith("png"):
-                    #print("Saved")
-                    urllib.request.urlretrieve(url, filename)
-                    found = True
-                    break
-    #print("Done.")
+                subreddit = await reddit.subreddit(post)
+                async for submission in subreddit.hot():
+                    if submission.permalink in sent:
+                        continue
+                    url = (submission.url)
+                    sent.append(submission.permalink)
+                    with open("sent.json", "w") as f:
+                        json.dump(sent, f)
+                    if url.endswith("jpg") or url.endswith("jpeg") or url.endswith("png"):
+                        urllib.request.urlretrieve(url, filename)                        
+                        found = True
+                        break
