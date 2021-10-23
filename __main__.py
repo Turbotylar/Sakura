68661f9f1d62059c3bd76d30358fb7c8b6d377f0
turbotyla <turbotylar@gmail.com>
2021-10-24 05:02:51 +1300

    Main changes

--- __main__.py
+++ __main__.py
@@ -1,69 +1,58 @@
+#Import all required libraries
 from __future__ import unicode_literals
-import os
 import discord
-from praw_ import praw_login
 import datetime
-#from discord.ext.commands.core import has_permissions, has_any_role
 from discord.ext import commands
 import asyncio
 from youtube_search import YoutubeSearch
 import json
 
-
 #Declare intents
 intents = discord.Intents.default()
 intents.members = True
 
-
+#Get Prefix from config
 def get_prefix(bot, message):
     prefixes = bot.config["prefixes"]
     return commands.when_mentioned_or(*prefixes)(bot, message)
 
+#List of cogs
 startup_cogs = [
     "cogs.error",
     "cogs.manage",
-    "cogs.music"
+    "cogs.music",
+    "cogs.redditgive"
 ]
 
-
-
+#Setup bot with prefix and intents
 bot = commands.Bot(command_prefix=get_prefix, intents=intents)
 
-
+#On startup print bots name and set presence
 @bot.event
 async def on_ready():
     print(f"Now logged in as {bot.user.name}")
     await bot.change_presence(status=discord.Status.online, activity=discord.Game("https://github.com/Turbotylar/LoliBot"))
 
-@bot.command()
-async def give(ctx, arg):
-    try:
-        if arg == 'loli':
-            praw_login('cutelittlefangs')
-            await ctx.send("Here is your loli: <:Lozic:854911358114332683>")
-            await ctx.send(file=discord.File('image.png'))
-        elif arg == 'cat':
-            praw_login('cat')
-            await ctx.send("Here is your cat:")
-            await ctx.send(file=discord.File('image.png'))
-        elif arg == 'kat':
-            praw_login('KatarinaMains')
-            await ctx.send("Here is your cat:")
-        elif arg == 'yuumi':
-            praw_login('YuumiMains')
-            await ctx.send("Here is your cat:")
-            await ctx.send(file=discord.File('image.png')) 
-    except Exception as e:
-        await ctx.send(e)
 
+#Load Extentions and commands
+if __name__ == "__main__":
+    for ext in startup_cogs:
+        bot.load_extension(ext)
+    
+    for cog_name in bot.cogs:
+        cog = bot.get_cog(cog_name)
+        for command in cog.get_commands():
+            print(command)
+
+#>today returns current day in NZT
 @bot.command()
 async def today(ctx):
     day = datetime.date.today()
     await ctx.send(day.strftime("%A %B %d %Y"))    
 
+#If perks speaks, mute him for 20 seconds
 @bot.listen()
 async def on_message(message):    
-    #If perks speaks, mute him for 20 seconds
     if str(message.author) == 'PerksOCE#7956':
         author = message.author
         muted = message.guild.get_role(875284225111248917)
@@ -71,6 +60,7 @@ async def on_message(message):
         await asyncio.sleep(20)
         await author.remove_roles(muted)
 
+#>find will search for a related video on youtube
 @bot.command()
 async def find(ctx, arg):
     search = str(arg)
