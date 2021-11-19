12201643fe5c3a9bee01abe231b0b0eea235e1bb
Thomas Hobson <thomas@hexf.me>
2021-11-20 02:39:05 +1300

    list commands

--- cogs/manage.py
+++ cogs/manage.py
@@ -4,7 +4,11 @@ import subprocess
 import typing
 import sys
 import importlib
+from os.path import dirname, basename, isfile, join
+import glob
 
+CHECKMARK=":white_check_mark:"
+CROSSMARK=":X:"
 
 class ManageCog(commands.Cog, name="Manage"):
     """
@@ -57,6 +61,17 @@ class ManageCog(commands.Cog, name="Manage"):
         self.client.unload_extension(cog)
         await ctx.send(f"Loaded {cog}")
 
+    @manage_cog.command(
+        name='list'
+    )
+    async def manage_cog_list(self, ctx, *, cog: str):
+        """Lists all cogs"""
+        module_files = glob.glob(join(dirname(__file__), "*.py"))
+        modules = [f"cogs.{basename(f)[:-3]}" for f in modules if isfile(f) and not f.endswith('__init__.py')]
+        
+        
+        await ctx.send(f"{self.client.extensions} {modules}")
+
     @manage.command(
         name='config'
     )
@@ -88,9 +103,9 @@ class ManageCog(commands.Cog, name="Manage"):
 
             try:
                 importlib.reload(module)
-                reloads.append(f"✔ {key}")
+                reloads.append(f"{CHECKMARK} {key}")
             except Exception as e:
-                reloads.append(f"✘ {key}: {type(e)}")
+                reloads.append(f"{CROSSMARK} {key}: {type(e)}")
         
         await ctx.send("Reloading Lolibot modules: \n" + "\n".join(reloads))
 
@@ -99,9 +114,9 @@ class ManageCog(commands.Cog, name="Manage"):
             
             try:
                 self.client.reload_extension(cog)
-                cog_reloads.append(f"✔ {cog}")
+                cog_reloads.append(f"{CHECKMARK} {cog}")
             except Exception as e:
-                cog_reloads.append(f"✘ {cog}: {type(e)}")
+                cog_reloads.append(f"{CROSSMARK} {cog}: {type(e)}")
             
         await ctx.send("Reloading cogs: \n" + "\n".join(cog_reloads) + "\n Done")
 
