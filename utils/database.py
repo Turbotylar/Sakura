3b97cc21ec09df6667a595515e50ec04ba598ae8
Thomas Hobson <thomas@hexf.me>
2021-11-20 01:11:28 +1300

    fix hooks

--- utils/database.py
+++ utils/database.py
@@ -7,18 +7,18 @@ logger = logging.getLogger(__name__)
 #   Before/After Invoke hooks
 #
 
-async def database_connect(ctx):
+async def database_connect(cog, ctx):
     """Before-hook to establish database connection"""
     logger.debug("Connected to database")
     ctx.db_session = ctx.bot.DBSession()
 
-async def database_cleanup(ctx):
+async def database_cleanup(cog, ctx):
     """After-hook to cleanup after database connection"""
     logger.debug("Commiting database changes")
     ctx.db_session.commit()
     logger.debug("Commited database changes")
 
-async def attach_user(ctx):
+async def attach_user(cog, ctx):
     """Before-hook to attach message author to ctx.db_user"""
     logger.debug("Getting user from database")
     ctx.db_user = get_user(ctx.db_session, ctx.author.id)
