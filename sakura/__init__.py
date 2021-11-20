e4a189decb95f9fc05dcf0db8ea45b6af6411a90
Thomas Hobson <git@hexf.me>
2021-11-20 14:49:31 +1300

    rework redit cog

---
+++ sakura/__init__.py
@@ -0,0 +1,2 @@
+
+USER_AGENT = "Sakura/1.0.0 DiscordBot/1.0.0"
--- sakura/cogs/reddit.py
+++ sakura/cogs/reddit.py
@@ -1,6 +1,7 @@
+import sakura
 import discord
 from discord.ext import commands
-from praw_ import praw_login
+import asyncpraw
 
 class Reddit(commands.Cog, name="Reddit"):
     """
@@ -9,22 +10,46 @@ class Reddit(commands.Cog, name="Reddit"):
     def __init__(self, client):
         self.client = client
 
+        self.previously_sent = []
+
+        praw_conf = self.client.config["praw"]
+
+        self.praw = asyncpraw.Reddit(
+            client_id = praw_conf["client_id"],
+            client_secret = praw_conf["client_secret"],
+
+            username = praw_conf["username"],
+            password = praw_conf["password"],
+
+            user_agent = sakura.USER_AGENT
+        )
+
     @commands.command(
         name="reddit",
         breif="Retrieve an image from reddit"
     )    
     async def reddit(self, ctx, arg):
         """Retrieve an image from reddit"""
-        try:
-            async with ctx.typing():
-                imgurl, title = await (praw_login(str(arg)))
+
+        async with ctx.typing():
+            subreddit = await self.praw.subreddit(str(arg))
+
+            imgurl = None
+            title = None
+
+            async for submission in subreddit.hot():
+                if submission.permalink not in self.previously_sent:
+                    imgurl = submission.url
+                    title = submission.title
+
+            if imgurl is None or title is None:
+                await ctx.send("Couldn't find any new posts on r/" + arg)
+            else:
                 embed = discord.Embed(title=("Here you go:"), color=0xeb34cf)
                 embed.set_image(url=imgurl)
                 embed.add_field(name=str(title), value=imgurl, inline=False)
+
                 await ctx.send(embed=embed)
-        except Exception as e:
-            await ctx.send("That isn't an option, if you think this is a mistake contact Turbotylar#7714")
-            await ctx.send(e)
 
 def setup(bot):
     bot.add_cog(Reddit(bot))
