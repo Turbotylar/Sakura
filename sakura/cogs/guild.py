0e57ac75d844c7b98572b270c0162afc3920befb
Tyler Mandich <turbotylar@gmail.com>
2021-11-26 00:23:55 +1300

    Update guild.py

--- sakura/cogs/guild.py
+++ sakura/cogs/guild.py
@@ -11,27 +11,34 @@ class Guild(commands.Cog, name="Guild"):
     """
     def __init__(self, client):
         self.client = client
-    
+
+        
     @guild_only()
     @database_connect
     @attach_database_guild
     @commands.Cog.listener()
-    async def on_member_join(ctx, member):
-        welcome_channel = ctx.guild.get_channel(ctx.db_guild.welcome_channel)
+    async def on_member_join(self, member):
+        database_session = member.bot.DBSession()
+        guild = database_session.query(Guild).filter_by(discord_id=member.guild.id).first()
+        welcome_channel = self.guild.get_channel(guild.welcome_channel)
+        database_session.close()
         embed=discord.Embed(title=f"Welcome to the server {member}!", color=0xeb34cf)
         embed.set_image(url=member.avatar_url)
         await welcome_channel.send(embed=embed)
     
+
     @guild_only()
     @database_connect
     @attach_database_guild
     @commands.Cog.listener()
-    async def on_member_remove(ctx, member):
-        welcome_channel = ctx.guild.get_channel(ctx.db_guild.welcome_channel)
+    async def on_member_remove(self, member):
+        database_session = member.bot.DBSession()
+        guild = database_session.query(Guild).filter_by(discord_id=member.guild.id).first()
+        welcome_channel = self.guild.get_channel(guild.welcome_channel)
+        database_session.close()
         embed=discord.Embed(title=f"{member} has left the server D:", color=0xeb34cf)
         embed.set_image(url=member.avatar_url)
         await welcome_channel.send(embed=embed)
-
 
 def setup(bot):
     bot.add_cog(Guild(bot))
