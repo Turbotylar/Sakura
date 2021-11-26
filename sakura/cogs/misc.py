baf8d0b04ae33d7261c69929184fa284711aff3a
Tyler Mandich <turbotylar@gmail.com>
2021-11-26 18:05:56 +1300

    Update misc.py

--- sakura/cogs/misc.py
+++ sakura/cogs/misc.py
@@ -53,6 +53,11 @@ class Misc(commands.Cog, name="Miscellaneous"):
         embed = discord.Embed(color=0xeb34cf)
         embed.set_image(url="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/aurora-bloom-2-jpg-1579817827.jpg")
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def jas(self, ctx):
+        embed = discord.Embed(title="Hi",color=0xf584ea)
+        await ctx.send(embed=embed)
         
         
 def setup(bot):
