7e328b9e229ca1a0f9f95914090d62a88691ac47
Thomas Hobson <git@hexf.me>
2021-11-28 05:02:50 +1300

    bot(guild): fix join/remove event handlers

--- sakura/cogs/guild.py
+++ sakura/cogs/guild.py
@@ -1,9 +1,7 @@
 import discord
 from discord.ext import commands
-from sakura.utils.checks import is_guild_moderator
-from sakura.utils.database import attach_database_guild, database_connect
+from sakura.models.guild import Guild as DBGuild
 from discord.ext.commands.core import guild_only
-
 
 class Guild(commands.Cog, name="Guild"):
     """
@@ -15,23 +13,34 @@ class Guild(commands.Cog, name="Guild"):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        database_session = member.bot.DBSession()
-        guild = database_session.query(Guild).filter_by(discord_id=member.guild.id).first()
-        welcome_channel = self.guild.get_channel(guild.welcome_channel)
+        database_session = self.client.DBSession()
+        
+        guild = database_session.query(DBGuild).filter_by(discord_id=member.guild.id).first()
+        welcome_channel = member.guild.get_channel(guild.welcome_channel)
+        
         database_session.close()
+        
         embed=discord.Embed(title=f"Welcome to the server {member}!", color=0xeb34cf)
-        embed.set_image(url=member.avatar_url)
+        if member.avatar is not None:
+            embed.set_image(url=member.avatar.url)
         await welcome_channel.send(embed=embed)
-    
+
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        database_session = member.bot.DBSession()
-        guild = database_session.query(Guild).filter_by(discord_id=member.guild.id).first()
-        welcome_channel = self.guild.get_channel(guild.welcome_channel)
+        database_session = self.client.DBSession()
+
+        guild = database_session.query(DBGuild).filter_by(discord_id=member.guild.id).first()
+        welcome_channel = member.guild.get_channel(guild.welcome_channel)
+        
         database_session.close()
+
+
         embed=discord.Embed(title=f"{member} has left the server D:", color=0xeb34cf)
-        embed.set_image(url=member.avatar_url)
+
+        if member.avatar is not None:
+            embed.set_image(url=member.avatar.url)
+
         await welcome_channel.send(embed=embed)
 
 def setup(bot):
